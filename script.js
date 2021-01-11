5797ec3eba8efadb54eef8289c95e1209feb180e
Tobi <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-01-11 14:48:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 let count = 0;
 //select value and buttons
 const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+const btns = document.querySelectorAll("#inc");
+const btn2 = document.querySelectorAll(".btn btn-outline-secondary");
 //Functionnality of the Program 
 btns.forEach(function(btn){
         btns[0].addEventListener("click",function(e){
             const styles = e.currentTarget.classList;
             if(styles.contains("btn")){
                 count++;
-            }else if(styles.contains("btn#Decrease")){
+            }else if(styles.contains("btn btn-outline-secondary")){
                 count--;
             }else{
                 count=0;
